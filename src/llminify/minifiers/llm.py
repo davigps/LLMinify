21f8f889b48bfc5ab74d7875ef81147ce888cecf
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-08-18 12:14:35 -0300

    feat: add terser on llm minifier

--- src/llminify/minifiers/llm.py
+++ src/llminify/minifiers/llm.py
@@ -1,23 +1,46 @@
+import os
+
 from langchain_core.output_parsers import StrOutputParser
 from llminify.available_models import get_model
 from llminify.minifiers.base import BaseMinifier
+from llminify.minifiers.terser import TerserMinifier
 from llminify.templates.minifier import minifier_prompt
 from llminify.templates.optimizer import optimizer_prompt
+from llminify.utils.timestamp import get_timestamp_string
 
 
 class LlmMinifier(BaseMinifier):
     def __init__(self, model: str) -> None:
+        self.terser_minifier = TerserMinifier()
         super().__init__(model)
 
+    def _save_on_temp_file(self, content: str) -> str:
+        file_path = f"./tmp-{get_timestamp_string()}"
+        with open(file_path, "w") as f:
+            f.write(content)
+        return file_path
+
     def minify_file(self, file_path: str) -> str:
+        content = self._read_file(file_path)
+        minified = self._minify_with_model(content)
+
+        tmp_file_path = self._save_on_temp_file(minified)
+
+        minified = self._minify_with_terser(tmp_file_path)
+        os.remove(tmp_file_path)
+
+        return minified
+
+    def _minify_with_model(self, content: str) -> str:
         model = get_model(self.tool_name)
 
         optimize_chain = optimizer_prompt | model | StrOutputParser()
         minify_chain = minifier_prompt | model | StrOutputParser()
 
         overall_chain = optimize_chain | minify_chain
-
-        content = self._read_file(file_path)
         output = overall_chain.invoke(input={"code": content})
 
         return output
+
+    def _minify_with_terser(self, file_path: str) -> str:
+        return self.terser_minifier.minify_file(file_path)
--- src/llminify/templates/minifier.py
+++ src/llminify/templates/minifier.py
@@ -1,14 +1,48 @@
 from langchain.prompts import PromptTemplate
 
 content = """
+You are a code minifier, you job is to minify the code provided without changing its objectives.
+Pay attention:
+- You should rewrite the code to be smaller
+- It can be worse in readability
+- You should use the same names for external functions
+- You should use the same names for external variables
+- You can use different indentation levels
+- You can use different code structures
+Only return the minified code, without any explanations or comments.
+
+Examples of minifications:
+
 Minify the following code:
 
-{code}
+```
+function findSum(numbers) {{
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {{
+    sum = sum + numbers[i];
+  }}
+  return sum;
+}}
+
+let result = 0;
+for (let i = 0; i < 1000000; i++) {{
+  result += findSum([1, 2, 3, 4, 5]);
+}}
+console.log(result);
+```
 
 Minified code:
+
+function findSum(n){{let s=0;for(let i=0;i<n.length;)s+=n[i++];return s}}let r=0;for(let i=0;i<1e6;)r+=findSum([1,2,3,4,5]);console.log(r)
+
+
+Minify the following code:
+
+```
+{code}
+```
 """
 
 minifier_prompt = PromptTemplate(
-    input_variables=["code"],
-    template=content,
+    input_variables=["code"], template=content, name="minifier_prompt"
 )
