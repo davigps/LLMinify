2fd88f57a5ebd9aa673171827c8ad878c2658d75
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-08-30 22:18:22 -0300

    feat: add more few shots

--- src/llminify/templates/minifier.py
+++ src/llminify/templates/minifier.py
@@ -38,6 +38,75 @@ Minified code:
 
 function findSum(n){{let s=0;for(let i=0;i<n.length;)s+=n[i++];return s}}let r=0;for(let i=0;i<1e6;)r+=findSum([1,2,3,4,5]);console.log(r)
 
+Minify the following code:
+
+```
+function factorial(n) {{
+  if (n === 0 || n === 1) {{
+    return 1;
+  }}
+  return n * factorial(n - 1);
+}}
+  
+console.log(factorial(5));
+
+class TaskManager {{
+  constructor() {{
+    this.tasks = [];
+  }}
+
+  addTask(task) {{
+    this.tasks.push(task);
+  }}
+
+  removeTask(index) {{
+    if (index >= 0 && index < this.tasks.length) {{
+      this.tasks.splice(index, 1);
+    }}
+  }}
+
+  getTask(index) {{
+    if (index >= 0 && index < this.tasks.length) {{
+      return this.tasks[index];
+    }}
+    return null;
+  }}
+
+  getAllTasks() {{
+    return this.tasks;
+  }}
+
+  clearAllTasks() {{
+    this.tasks = [];
+  }}
+
+  getTaskCount() {{
+    return this.tasks.length;
+  }}
+
+  updateTask(index, newTask) {{
+    if (index >= 0 && index < this.tasks.length) {{
+      this.tasks[index] = newTask;
+    }}
+  }}
+
+  sortTasks() {{
+    this.tasks.sort();
+  }}
+
+  filterTasks(predicate) {{
+    return this.tasks.filter(predicate);
+  }}
+
+  forEachTask(callback) {{
+    this.tasks.forEach(callback);
+  }}
+}}
+```
+
+Minified code:
+
+function factorial(n){{if(n===0||n===1){{return 1}}return n*factorial(n-1)}}console.log(factorial(5));class TaskManager{{constructor(){{this.tasks=[]}}addTask(task){{this.tasks.push(task)}}removeTask(index){{if(index>=0&&index<this.tasks.length){{this.tasks.splice(index,1)}}}}getTask(index){{if(index>=0&&index<this.tasks.length){{return this.tasks[index]}}return null}}getAllTasks(){{return this.tasks}}clearAllTasks(){{this.tasks=[]}}getTaskCount(){{return this.tasks.length}}updateTask(index,newTask){{if(index>=0&&index<this.tasks.length){{this.tasks[index]=newTask}}}}sortTasks(){{this.tasks.sort()}}filterTasks(predicate){{return this.tasks.filter(predicate)}}forEachTask(callback){{this.tasks.forEach(callback)}}}}
 
 Minify the following code:
 
